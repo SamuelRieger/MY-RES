7bd7edaa4338881ce684edc4f1ca903032acd4c9
Samuel Rieger <29229366+SamuelRieger@users.noreply.github.com>
2024-07-01 12:01:46 +1000

    Added animate function to random walk.

--- random_walk.R
+++ random_walk.R
@@ -41,3 +41,4 @@ sigma <- 0.5
 samples <- random_walk_metropolis_hastings(n, start_x, start_y, sigma)
 
 plot(samples$x, samples$y, pch = 20, col = rgb(0, 0, 0, 0.1), main = "Random Walk Metropolis-Hastings Samples", xlab = "x", ylab = "y")
+animate_mcmc(samples, "Random Walk Metropolis-Hastings Samples")
